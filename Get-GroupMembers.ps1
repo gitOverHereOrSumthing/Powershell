0b87c1e3370cdb874ba12523f963700bd311e156
Simon Goodson <sgoodson@huntswood.com>
2019-08-16 07:59:33 +0100

    Added help comments to a number of scripts

--- Get-GroupMembers.ps1
+++ Get-GroupMembers.ps1
@@ -1,12 +1,35 @@
-param([string] $Group = "Domain Admins", [string] $OutputFile = "None", [string] $Recursive = "false")
+<#
+	.Synopsis
+		Lists all of the members of a group
+	.Description
+		Lists all of the members of a group.  Use the Recursive switch to list the members of groups within the group.
+	.Example
+		Get-GroupMembers -Group "Domain Admins"
+	.Example
+		Get-GroupMembers -Recursive -Group "Domain Admins"
+	.Example
+		Get-GroupMembers -Group "Domain Admins" -OutputFile "c:\temp\output.txt"
+#>
 
-if ($Recursive -eq "false"){
+[CmdletBinding()]
+Param(
+	# The name of the Ad group to search
+	[Parameter(mandatory=$true)]
+	[string] $Group = "", 
+	# The output file name if required
+	[string] $OutputFile = "", 
+	# Switch to list group members within the group you are searching
+	[switch] $Recursive
+	)
+
+# Body of script
+if ($Recursive -eq $false){
 	$users = get-adgroupmember -Identity $Group  | sort-object
 } Else {
 	$users = get-adgroupmember -Identity $Group -Recursive  | sort-object
 }
 
-if ($OutputFile -ne "None"){
+if ($OutputFile -ne ""){
 	if (Test-Path -Path $OutputFile){
 		Remove-item $OutputFile
 	}
@@ -15,7 +38,7 @@ if ($OutputFile -ne "None"){
 foreach($user in $users) {
     $output = $user.Name
 	
-	if ($OutputFile -ne "None"){
+	if ($OutputFile -ne ""){
 		add-content $OutputFile $output;
 	}
 	echo $output
--- Get-HostByIPAddress.ps1
+++ Get-HostByIPAddress.ps1
@@ -1,2 +1,17 @@
-$ComputerName = $args[0]
-[System.Net.Dns]::GetHostByAddress($ComputerName)
+<#
+	.Synopsis
+		Searches DNS for the name of the computer with the passed IP address
+	.Description
+		Searches DNS for the name of the computer with the passed IP address
+	.Example
+		Get-HostByIPAddress 172.16.0.1
+#>
+
+[CmdletBinding()]
+Param (
+	# The IP address of the computer you want to search for
+	$IPAddress
+)
+
+# Body of script
+[System.Net.Dns]::GetHostByAddress($IPAddress)
